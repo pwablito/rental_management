2d54da6b1e4b00db1664055ed3d71754dfbb29de
pwablito <pwablito@gmail.com>
2021-07-02 11:21:32 -0600

    Cleanup and redirect / to index.html

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, redirect
 import api.api
 
 app = Flask(__name__, static_url_path="", static_folder="client/dist")
 
 app.add_url_rule('/api/test', view_func=api.api.test)
+
+@app.route('/')
+def index():
+    return redirect("/index.html", code=302)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
