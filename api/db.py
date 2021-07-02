5f9acaf6bc0b3bc27235df27fc38f1fa14c75da1
pwablito <pwablito@gmail.com>
2021-07-02 17:59:13 -0600

    User db operations

--- api/db.py
+++ api/db.py
@@ -1,18 +1,20 @@
 import sqlite3
+import user
+import error
 
 
 CLIENT_TYPE = 1
 REALTOR_TYPE = 2
 ADMIN_TYPE = 3
 
-def get_connection(db_file):
+def get_connection(db_file="db.sqlite"):
     try:
         return sqlite3.connect(db_file)
     except sqlite3.Error as e:
         print("Unable to connect to database: {}".format(e))
 
 def setup_database():
-    with get_connection("db.sqlite") as conn:
+    with get_connection() as conn:
         cursor = conn.cursor()
         cursor.execute(
             '''
@@ -43,3 +45,62 @@ def setup_database():
             )
             '''
         )
+
+def get_user_by_username(username):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        '''
+        SELECT id, username, name, created_on, type, password_hash, password_salt
+        FROM user WHERE username=?
+        ''', (username,)
+    )
+    row = cursor.fetchone()
+    if not row:
+        raise error.UserNotFoundException
+    user = user.User(row[0], row[1], row[2], row[4], row[5])
+    if row[3] == CLIENT_TYPE:
+        return user.ClientUser(user)
+    else if row[3] == REALTOR_TYPE:
+        return user.RealtorUser(user)
+    else if row[3] == ADMIN_TYPE:
+        return user.AdminUser(user)
+    raise InvalidUserTypeException
+
+def get_user_by_username(id):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        '''
+        SELECT id, username, name, created_on, type, password_hash, password_salt
+        FROM user WHERE id=?
+        ''', (id,)
+    )
+    row = cursor.fetchone()
+    if not row:
+        raise error.UserNotFoundException
+    if row[3] == CLIENT_TYPE:
+        return user.ClientUser(user)
+    else if row[3] == REALTOR_TYPE:
+        return user.RealtorUser(user)
+    else if row[3] == ADMIN_TYPE:
+        return user.AdminUser(user)
+
+def get_user_type_number(user):
+    if type(user) == user.ClientUser:
+        return CLIENT_TYPE
+    if type(user) == user.RealtorUser:
+        return REALTOR_TYPE
+    if type(user) == user.AdminType:
+        return ADMIN_TYPE
+    raise error.InvalidUserTypeException
+
+def insert_user(user):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        '''
+        INSERT INTO user (id, username, name, created_on, type, password_hash, password_salt)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+        ''', (user.id, user.username, user.name, user.created_on, get_user_type_number(user), user.password_hash, user.password_salt)
+    )
---
+++ api/user.py
@@ -0,0 +1,40 @@
+import util
+
+class User:
+    def __init__(self, id, username, created_on, password_hash, password_salt):
+        self.id = id
+        self.username = username
+        self.created_on = created_on
+        self.password_hash = password_hash
+        self.password_salt = password_salt
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.usename,
+            "created_on": self.created_on,
+        }
+
+    def authenticate(self, password):
+        return util.get_hash(password + self.password_salt) == self.password_hash
+
+
+class ClientUser(User):
+    def to_dict(self):
+        return super().to_dict().update({
+            "type": "client"
+        })
+
+
+class RealtorUser(User):
+    def to_dict(self):
+        return super().to_dict().update({
+            "type": "realtor"
+        })
+
+
+class AdminUser(User):
+    def to_dict(self):
+        return super().to_dict().update({
+            "type": "admin"
+        })
