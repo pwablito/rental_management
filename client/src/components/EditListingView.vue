a243fe6a0d9c259e08bc63aa5f28673839e96fde
pwablito <pwablito@gmail.com>
2021-07-13 20:47:45 -0600

    Front end update listings

--- client/src/components/EditListingView.vue
+++ client/src/components/EditListingView.vue
@@ -1,96 +1,168 @@
 <template>
   <div id="editlistingview">
-    <div v-if="!this.editing">
-      <div class="row">
-        <div class="col">
-          {{ listing.name }}
-        </div>
-        <div class="col">
-          {{ listing.description }}
-        </div>
-        <div class="col">
-          {{ listing.floor_area }}
-        </div>
-        <div class="col">
-          {{ listing.price }}
-        </div>
-        <div class="col">{{ listing.rooms }}/{{ listing.bathrooms }}</div>
-        <div class="col">
-          {{ listing.created_on }}
-        </div>
-        <div class="col">{{ listing.latitude }}/{{ listing.longitude }}</div>
-        <div class="col">
-          <button @click="start_editing" class="btn btn-secondary">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-pencil"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"
-              />
-            </svg>
-          </button>
-        </div>
-        <div class="col">
-          <button @click="delete_listing" class="btn btn-danger">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-trash"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"
-              />
-              <path
-                fill-rule="evenodd"
-                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-              />
-            </svg>
-          </button>
-        </div>
+    <div class="row">
+      <div class="col-1 item">
+        {{ listing.name }}
+      </div>
+      <div class="col-2 item">
+        {{ listing.description }}
+      </div>
+      <div class="col-1 item">{{ listing.floor_area }} sq.ft.</div>
+      <div class="col-1 item">${{ listing.price }}</div>
+      <div class="col-1 item">{{ listing.rooms }}/{{ listing.bathrooms }}</div>
+      <div class="col-2 item">
+        {{ this.listing_created }}
+      </div>
+      <div class="col-1 item">
+        {{ listing.latitude }}/{{ listing.longitude }}
+      </div>
+      <div class="col-1 item">
+        <div v-if="listing.is_listed">X</div>
+      </div>
+      <div class="col-2 item">
+        <button @click="$bvModal.show(modal_id)" class="btn btn-secondary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-pencil"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"
+            />
+          </svg>
+        </button>
+        <button @click="delete_listing" class="btn btn-danger">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-trash"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"
+            />
+            <path
+              fill-rule="evenodd"
+              d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+            />
+          </svg>
+        </button>
       </div>
     </div>
-    <div v-else>
-      Edit the listing here
-      <button class="btn btn-danger" @click="cancel_editing">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          class="bi bi-x"
-          viewBox="0 0 16 16"
-        >
-          <path
-            d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
-          />
-        </svg>
-      </button>
-      <button class="btn btn-success" @click="update_listing">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          class="bi bi-check"
-          viewBox="0 0 16 16"
-        >
-          <path
-            d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"
-          />
-        </svg>
-      </button>
-    </div>
+
     <p class="red centered" v-if="this.error_message">
       {{ this.error_message }}
     </p>
+
+    <b-modal :id="modal_id" title="Edit Listing" hide-footer>
+      <div class="form-group">
+        <label for="listing_name">Name</label>
+        <input
+          type="text"
+          v-model="edit_listing.name"
+          class="form-control"
+          id="listing_name"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <label for="listing_description">Description</label>
+        <input
+          type="text"
+          v-model="edit_listing.description"
+          class="form-control"
+          id="listing_description"
+        />
+      </div>
+      <div class="form-group">
+        <label for="listing_floor_area">Floor Area (square feet)</label>
+        <input
+          type="number"
+          min="0"
+          max="1000000"
+          v-model="edit_listing.floor_area"
+          class="form-control"
+          id="listing_floor_area"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <label for="listing_price">Price per Month in USD</label>
+        <input
+          type="number"
+          min="0"
+          max="1000000"
+          v-model="edit_listing.price"
+          class="form-control"
+          id="listing_price"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <label for="listing_rooms"># Rooms</label>
+        <input
+          type="number"
+          min="0"
+          max="100"
+          v-model="edit_listing.rooms"
+          class="form-control"
+          id="listing_rooms"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <label for="listing_bathrooms"># Bathrooms</label>
+        <input
+          type="number"
+          min="0"
+          max="1000"
+          v-model="edit_listing.bathrooms"
+          class="form-control"
+          id="listing_bathrooms"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <label for="listing_latitude">Latitude</label>
+        <input
+          type="number"
+          min="-90"
+          max="90"
+          step="0.0001"
+          v-model="edit_listing.latitude"
+          class="form-control"
+          id="listing_latitude"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <label for="listing_longitude">Longitude</label>
+        <input
+          type="number"
+          min="-180"
+          max="180"
+          step="0.0001"
+          v-model="edit_listing.longitude"
+          class="form-control"
+          id="listing_longitude"
+          required
+        />
+      </div>
+      <div class="form-group">
+          
+        <button class="btn btn-danger" @click="reset_edits">
+          Reset
+        </button>
+        <button class="btn btn-success" @click="update_listing">
+          Finished
+        </button>
+      </div>
+    </b-modal>
   </div>
 </template>
 
@@ -101,7 +173,6 @@ export default {
   name: "EditListingView",
   data() {
     return {
-      editing: false,
       edit_listing: {},
       loading: false,
       error_message: null,
@@ -111,9 +182,21 @@ export default {
     listing: Object,
     token: String,
   },
+  computed: {
+    modal_id() {
+      return this.listing.id + "-modal";
+    },
+    listing_created() {
+      return new Date(this.listing.created_on).toLocaleDateString(undefined, {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      });
+    },
+  },
   methods: {
     reset() {
-      this.edit_listing = this.listing;
+      this.edit_listing = Object.assign({}, this.listing);
     },
     update_listing() {
       this.loading = true;
@@ -126,6 +209,7 @@ export default {
         .then((response) => {
           if (response.data.success) {
             this.listing = this.edit_listing;
+            this.$emit("update_listings")
           } else {
             this.error_message = response.data.message;
           }
@@ -153,28 +237,31 @@ export default {
           }
         })
         .catch((err) => {
-          console.log(err)
+          console.log(err);
           this.error_message = "Something went wrong";
         })
         .then(() => {
           this.loading = false;
         });
     },
-    start_editing() {
-      this.editing = true;
-    },
-    cancel_editing() {
-      this.edit_listing = this.listing;
-      this.editing = false;
+    reset_edits() {
+      this.edit_listing = Object.assign({}, this.listing);
     },
   },
   created() {
-    this.edit_listing = this.listing;
+    this.edit_listing = Object.assign({}, this.listing);
   },
 };
 </script>
 
 <style>
+#editlistingview {
+  font-size: 10px;
+}
+.item {
+  padding-left: 0px !important;
+  padding-right: 0px !important;
+}
 .red {
   color: red;
 }
--- client/src/components/EditListingsView.vue
+++ client/src/components/EditListingsView.vue
@@ -1,7 +1,124 @@
 <template>
   <div id="editlistings" class="centered">
+    <div id="filters" class="centered">
+
+      <div class="row">
+        <div class="col-6">
+          <div class="input-group input-group-sm ">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-sm">Min Floor Area</span>
+            </div>
+            <input
+              type="number"
+              class="form-control"
+              aria-label="Min Floor Area"
+              aria-describedby="inputGroup-sizing-sm"
+              v-model="filters.floor_area_min"
+            />
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="input-group input-group-sm ">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-sm">Max Floor Area</span>
+            </div>
+            <input
+              type="number"
+              class="form-control"
+              aria-label="Max Floor Area"
+              aria-describedby="inputGroup-sizing-sm"
+              v-model="filters.floor_area_max"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-6">
+          <div class="input-group input-group-sm ">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-sm">Min Price</span>
+            </div>
+            <input
+              type="number"
+              class="form-control"
+              aria-label="Min Price"
+              aria-describedby="inputGroup-sizing-sm"
+              v-model="filters.price_min"
+            />
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="input-group input-group-sm ">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-sm">Max Price</span>
+            </div>
+            <input
+              type="number"
+              class="form-control"
+              aria-label="Max Price"
+              aria-describedby="inputGroup-sizing-sm"
+              v-model="filters.price_max"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-6">
+          <div class="input-group input-group-sm ">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-sm">Min Rooms</span>
+            </div>
+            <input
+              type="number"
+              class="form-control"
+              aria-label="Min Rooms"
+              aria-describedby="inputGroup-sizing-sm"
+              v-model="filters.bed_min"
+            />
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="input-group input-group-sm ">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-sm">Max Rooms</span>
+            </div>
+            <input
+              type="number"
+              class="form-control"
+              aria-label="Max Rooms"
+              aria-describedby="inputGroup-sizing-sm"
+              v-model="filters.bed_max"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <br><br>
+    
+    <div class="row">
+      <div class="col-1 item">Name</div>
+      <div class="col-2 item">Description</div>
+      <div class="col-1 item">Floor Area</div>
+      <div class="col-1 item">Price</div>
+      <div class="col-1 item">Bed/Bath</div>
+      <div class="col-2 item">Created</div>
+      <div class="col-1 item">Lat/Lon</div>
+      <div class="col-1 item">Listed</div>
+      <div class="col-2 item">Edit/Delete</div>
+    </div>
+    <hr />
     <div v-for="listing in this.listings" :key="listing.id">
-      <EditListingView :listing="listing" :token="token" @update_listings="get_listings" />
+      <div v-if="satisfies_filters(listing)">
+        <EditListingView
+          :listing="listing"
+          :token="token"
+          @update_listings="get_listings"
+        />
+        <hr />
+      </div>
     </div>
 
     <button class="btn btn-primary" @click="this.get_listings">
@@ -25,12 +142,56 @@ export default {
   data() {
     return {
       listings: [],
+      filters: {
+        floor_area_min: null,
+        floor_area_max: null,
+        price_min: null,
+        price_max: null,
+        bed_min: null,
+        bed_max: null,
+      },
     };
   },
   created() {
     this.get_listings();
   },
   methods: {
+    satisfies_filters(listing) {
+      if (listing === null) {
+        return false;
+      }
+      if (this.filters.floor_area_min !== null) {
+        if (listing.floor_area < this.filters.floor_area_min) {
+          return false;
+        }
+      }
+      if (this.filters.floor_area_max !== null) {
+        if (listing.floor_area > this.filters.floor_area_max) {
+          return false;
+        }
+      }
+      if (this.filters.price_min !== null) {
+        if (listing.price < this.filters.price_min) {
+          return false;
+        }
+      }
+      if (this.filters.price_max !== null) {
+        if (listing.price > this.filters.price_max) {
+          return false;
+        }
+      }
+      if (this.filters.bed_min !== null) {
+        if (listing.rooms < this.filters.bed_min) {
+          return false;
+        }
+      }
+      if (this.filters.bed_max !== null) {
+        if (listing.rooms > this.filters.bed_max) {
+          return false;
+        }
+      }
+      return true;
+    },
     get_listings() {
       this.loading = true;
       this.error_message = null;
@@ -57,6 +218,14 @@ export default {
 </script>
 
 <style>
+#editlistingsview {
+  font-size: 11px;
+  font-weight: bold;
+}
+.item {
+  padding-left: 0px !important;
+  padding-right: 0px !important;
+}
 .centered {
   text-align: center;
 }
