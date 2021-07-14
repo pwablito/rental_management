b4adac9766ea535bb3b8d0ce278facf58c443e4d
pwablito <pwablito@gmail.com>
2021-07-13 23:51:57 -0600

    Better listing browsing format

--- client/src/components/BrowseView.vue
+++ client/src/components/BrowseView.vue
@@ -1,37 +1,130 @@
 <template>
   <div id="browseview" class="centered">
-    <b-tabs content-class="mt-3" align="center" pills>
-      <b-tab title="List" active>
-        <div id="loading_div" class="centered" v-if="this.loading">Loading</div>
-        <div v-else>
-          <p>Listings:</p>
-          <p v-if="this.listings.length === 0">None found</p>
-          <ul v-else class="left-aligned">
-            <div v-for="listing in this.listings"
-                :key="listing.id">
-              <ListingView :listing="listing" />
-              <hr>
+    <div id="filters" class="centered">
+
+      <div class="row">
+        <div class="col-6">
+          <div class="input-group input-group-sm ">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-sm">Min Floor Area</span>
             </div>
-          </ul>
-          <p class="red" v-if="this.error_message !== ''">
-            {{ this.error_message }}
-          </p>
-          <button class="btn btn-primary" @click="this.get_listings">
-            Reload
-          </button>
+            <input
+              type="number"
+              class="form-control"
+              aria-label="Min Floor Area"
+              aria-describedby="inputGroup-sizing-sm"
+              v-model="filters.floor_area_min"
+            />
+          </div>
         </div>
-      </b-tab>
-      <b-tab title="Map">
-        <MapListingsView :listings="listings" />
-      </b-tab>
-    </b-tabs>
+        <div class="col-6">
+          <div class="input-group input-group-sm ">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-sm">Max Floor Area</span>
+            </div>
+            <input
+              type="number"
+              class="form-control"
+              aria-label="Max Floor Area"
+              aria-describedby="inputGroup-sizing-sm"
+              v-model="filters.floor_area_max"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-6">
+          <div class="input-group input-group-sm ">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-sm">Min Price</span>
+            </div>
+            <input
+              type="number"
+              class="form-control"
+              aria-label="Min Price"
+              aria-describedby="inputGroup-sizing-sm"
+              v-model="filters.price_min"
+            />
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="input-group input-group-sm ">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-sm">Max Price</span>
+            </div>
+            <input
+              type="number"
+              class="form-control"
+              aria-label="Max Price"
+              aria-describedby="inputGroup-sizing-sm"
+              v-model="filters.price_max"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-6">
+          <div class="input-group input-group-sm ">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-sm">Min Rooms</span>
+            </div>
+            <input
+              type="number"
+              class="form-control"
+              aria-label="Min Rooms"
+              aria-describedby="inputGroup-sizing-sm"
+              v-model="filters.bed_min"
+            />
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="input-group input-group-sm ">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-sm">Max Rooms</span>
+            </div>
+            <input
+              type="number"
+              class="form-control"
+              aria-label="Max Rooms"
+              aria-describedby="inputGroup-sizing-sm"
+              v-model="filters.bed_max"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <br><br>
+
+    <div class="row left-aligned">
+      <div class="col-1 item">Name</div>
+      <div class="col-1 item">Realtor</div>
+      <div class="col-3 item">Description</div>
+      <div class="col-1 item">Floor Area</div>
+      <div class="col-1 item">Price</div>
+      <div class="col-2 item">Bed/Bath</div>
+      <div class="col-2 item">Created</div>
+      <div class="col-1 item">Lat/Lon</div>
+    </div>
+    <hr />
+    <div v-for="listing in this.listings" :key="listing.id" class="left-aligned">
+      <div v-if="satisfies_filters(listing)">
+        <ListingView :listing="listing" />
+        <hr>
+      </div>
+    </div>
+
+    <button class="btn btn-primary" @click="this.get_listings">
+      Reload
+    </button>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import ListingView from "./ListingView.vue";
-import MapListingsView from "./MapListingsView.vue";
 
 export default {
   name: "BrowseView",
@@ -40,19 +133,60 @@ export default {
   },
   components: {
     ListingView,
-    MapListingsView
   },
   data() {
     return {
       listings: [],
-      loading: false,
-      error_message: "",
+      filters: {
+        floor_area_min: null,
+        floor_area_max: null,
+        price_min: null,
+        price_max: null,
+        bed_min: null,
+        bed_max: null,
+      },
     };
   },
   created() {
     this.get_listings();
   },
   methods: {
+    satisfies_filters(listing) {
+      if (listing === null) {
+        return false;
+      }
+      if (this.filters.floor_area_min !== null) {
+        if (listing.floor_area < this.filters.floor_area_min) {
+          return false;
+        }
+      }
+      if (this.filters.floor_area_max !== null) {
+        if (listing.floor_area > this.filters.floor_area_max) {
+          return false;
+        }
+      }
+      if (this.filters.price_min !== null) {
+        if (listing.price < this.filters.price_min) {
+          return false;
+        }
+      }
+      if (this.filters.price_max !== null) {
+        if (listing.price > this.filters.price_max) {
+          return false;
+        }
+      }
+      if (this.filters.bed_min !== null) {
+        if (listing.rooms < this.filters.bed_min) {
+          return false;
+        }
+      }
+      if (this.filters.bed_max !== null) {
+        if (listing.rooms > this.filters.bed_max) {
+          return false;
+        }
+      }
+      return true;
+    },
     get_listings() {
       this.loading = true;
       this.error_message = null;
@@ -80,13 +214,13 @@ export default {
 </script>
 
 <style>
-.red {
-  color: red;
-}
 .centered {
   text-align: center;
 }
 .left-aligned {
   text-align: left;
+}
+.red {
+  color: red;
 }
 </style>
--- client/src/components/ListingView.vue
+++ client/src/components/ListingView.vue
@@ -1,44 +1,139 @@
 <template>
-  <div id="listingview">
-    <div>
-        <p>Name: {{this.listing.name}}</p>
-        <p>Description: {{this.listing.description}}</p>
-        <p>Floor Area: {{this.listing.floor_area}}</p>
-        <p>Price: {{this.listing.price}}</p>
-        <p>Rooms: {{this.listing.rooms}}</p>
-        <p>Bathrooms: {{this.listing.bathrooms}}</p>
-        <p>Latitude: {{this.listing.latitude}}</p>
-        <p>Longitude: {{this.listing.longitude}}</p>
-        <p>Created on: {{this.created_on_date}}</p>
-
+  <div id="editlistingview">
+    <div class="row">
+      <div class="col-1 item">
+        {{ listing.name }}
+      </div>
+      <div class="col-1 item">
+        {{ listing.realtor }}
+      </div>
+      <div class="col-3 item">
+        {{ listing.description }}
+      </div>
+      <div class="col-1 item">{{ listing.floor_area }} sq.ft.</div>
+      <div class="col-1 item">${{ listing.price }}</div>
+      <div class="col-2 item">{{ listing.rooms }}/{{ listing.bathrooms }}</div>
+      <div class="col-2 item">
+        {{ this.listing_created }}
+      </div>
+      <div class="col-1 item">
+        {{ listing.latitude }}/{{ listing.longitude }}
+      </div>
+      <div class="col-1 item">
+        <div v-if="listing.is_listed">X</div>
+      </div>
     </div>
   </div>
 </template>
+
 <script>
+import axios from "axios";
 
 export default {
-  name: "ListingView",
+  name: "EditListingView",
   data() {
     return {
+      editing: false,
+      edit_listing: {},
       loading: false,
+      error_message: null,
     };
   },
   props: {
-      listing: Object,
+    listing: Object,
+    token: String,
   },
   computed: {
-      created_on_date() {
-        return Date.parse(this.listing.created_on).toLocaleString();
-      }
-  }
+    modal_id() {
+      return this.listing.id + "-modal";
+    },
+    listing_created() {
+      return new Date(this.listing.created_on).toLocaleDateString(undefined, {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      });
+    },
+  },
+  methods: {
+    reset() {
+      this.edit_listing = Object.assign({}, this.listing);
+    },
+    update_listing() {
+      this.loading = true;
+      this.error_message = null;
+      axios
+        .post("/api/update_listing", {
+          listing: this.edit_listing,
+          token: this.token,
+        })
+        .then((response) => {
+          if (response.data.success) {
+            this.toggle_editing();
+            this.$emit("update_listings");
+          } else {
+            this.error_message = response.data.message;
+          }
+        })
+        .catch(() => {
+          this.error_message = "Something went wrong";
+        })
+        .then(() => {
+          this.loading = false;
+        });
+    },
+    submit_update_listing(e) {
+      e.preventDefault();
+      this.update_listing();
+    },
+    delete_listing() {
+      this.loading = true;
+      this.error_message = null;
+      axios
+        .post("/api/delete_listing", {
+          id: this.listing.id,
+          token: this.token,
+        })
+        .then((response) => {
+          if (response.data.success) {
+            this.$emit("update_listings");
+          } else {
+            this.error_message = response.data.message;
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          this.error_message = "Something went wrong";
+        })
+        .then(() => {
+          this.loading = false;
+        });
+    },
+    reset_edits() {
+      this.edit_listing = Object.assign({}, this.listing);
+    },
+    toggle_editing() {
+      this.editing = !this.editing;
+    },
+  },
+  created() {
+    this.edit_listing = Object.assign({}, this.listing);
+  },
 };
 </script>
 
 <style>
+#editlistingview {
+  font-size: 10px;
+}
+.item {
+  padding-left: 0px !important;
+  padding-right: 0px !important;
+}
 .red {
-    color: red;
+  color: red;
 }
 .centered {
-    text-align: center;
+  text-align: center;
 }
 </style>
